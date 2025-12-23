f4415a05c945df9cdeb8c6e8de857970bdb8f8e8
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-12-23 17:53:51 +0100

    feat(auth): add custom user model based on AbstractUser

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "full_name"]
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.email
--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "auth_app.CustomUser"
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
